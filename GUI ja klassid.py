8e5a5eac9e0a3094bdebb76299a66a5f82919a4d
mattiasp-it <mattias.paap@opilane.hariduskeskus.ee>
2025-12-16 09:28:03 +0200

    Update GUI ja klassid.py

--- GUI ja klassid.py
+++ GUI ja klassid.py
@@ -1,10 +1,12 @@
 # 
 class Karakter: 
-    def __init__(self, Nimi, Elud, Tugevus):
+    def __init__(self, Nimi, Elud, Tugevus, x, y):
         self.Nimi = Nimi
         self.Elud = Elud
         self.Tugevus = Tugevus
-
+        self.x = x
+        self.y = y
+        
     def KaotaElusi(self, kaotatudelud):
         if kaotatudelud <= 0:
             print("Ei sa kaotada vähem kui 1 elu")
@@ -19,8 +21,26 @@ class Karakter:
             self.Elud += kaotatudelud
             print(f"[{self.Nimi}] Lisati {kaotatudelud} elu.")
 
+    def LiiguYles(self)
+        self.y += 1
+        print(f"[{self.Nimi}] Liikusid ülesse")
+
+    def LiiguAlla(self)
+        self.y -= 1
+        print(f"[{self.Nimi}] Liikusid alla")
+
+    def LiiguVasakule(self)
+        self.x -= 1
+        print(f"[{self.Nimi}] Liikusid vasakule")
+
+     def LiiguParemale(self)
+        self.x += 1
+        print(f"[{self.Nimi}] Liikusid paremale")   
+
+
+
 class Vaenlane: 
-    def __init__(self, Elud, Tugevus):
+    def __init__(self, Elud, Tugevus, x, y):
         self.Elud = Elud
         self.Tugevus = Tugevus
 
