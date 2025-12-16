045cb7cf6fb731af3764109d80912329c691b4ca
mattiasp-it <mattias.paap@opilane.hariduskeskus.ee>
2025-12-16 09:19:12 +0200

    Update GUI ja klassid.py

--- GUI ja klassid.py
+++ GUI ja klassid.py
@@ -10,3 +10,23 @@ class Karakter:
             print("Ei sa kaotada vähem kui 1 elu")
         else:
             self.Elud -= kaotatudelud
+            print(f"[{self.Nimi}] Kaotasid {kaotatudelud} elu.")
+    
+    def LisaElusi(self, lisatudelud):
+        if lisatudelud <= 0:
+            print("Ei sa lisada vähem kui 1 elu")
+        else:
+            self.Elud += kaotatudelud
+            print(f"[{self.Nimi}] Lisati {kaotatudelud} elu.")
+
+class Vaenlane: 
+    def __init__(self, Elud, Tugevus):
+        self.Elud = Elud
+        self.Tugevus = Tugevus
+
+    def KaotaElusi(self, kaotatudelud):
+        if kaotatudelud <= 0:
+            print("Ei sa kaotada vähem kui 1 elu")
+        else:
+            self.Elud -= kaotatudelud
+            print(f"[Vaenlane] Kaotas {kaotatudelud} elu.")
