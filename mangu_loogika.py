7df8e3b0a2a86d0ad8b487195b38cf1133d1ee50
mustikak6rv <ander.kaasma@opilane.hariduskeskus.ee>
2025-12-16 09:34:09 +0200

    sd

---
+++ mangu_loogika.py
@@ -0,0 +1,33 @@
+from Karakter import Karakter
+from Vaenlane import Vaenlane
+
+
+
+import random
+randx = random.randint(1,5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if Karakter.elud == 0:
+    print ("Sa surid, vaenlased võitsid") and exit()
+
+
+
+
+
+tegelane = Karakter("Trevori", 100, 20, randx)
+
+tegelane1 = Vaenlane(30, 40)
+tegelane2 = Vaenlane(60, 55)
